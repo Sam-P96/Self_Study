e89d8123b551b9df65e16474ef2f3cdd2df6f6ba
Samnap Peo <Samnappeo@gmail.com>
2025-09-07 19:40:30 +0300

    Just an exercise

--- 002_experiments.py
+++ 002_experiments.py
@@ -4,11 +4,9 @@ word_count = {}
 # Text string
 text = "Later in the course, you'll see how to use the collections Counter class."
 
-# Iterate over every character in the string.
-for char in text.casefold():
-    # We're only counting letters and digits (no punctuation).
-    if char.isalnum():
-        word_count[char] = word_count.setdefault(char, 0) + 1
+for i in text.casefold():
+    if i.isalnum():
+        word_count[i] = word_count.setdefault(i, 0) + 1
 
 # Printing the dictionary
 for letter, count in sorted(word_count.items()):
